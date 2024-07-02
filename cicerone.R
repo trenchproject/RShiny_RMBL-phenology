af980ec28ba122fc084984c28c313ccb50726e8f
Ziliang Huang <zilianghuang@Ziliangs-MacBook-Pro.local>
2024-07-01 18:10:53 -0700

    Updated UI/Remove Broken Links

--- cicerone.R
+++ cicerone.R
@@ -1,75 +1,78 @@
 guide <- Cicerone$
   new()$
   step(
-    el = "viz-wrapper",
-    title = "Visualization",
-    description = HTML("Welcome to our RMBL phenology visualization tool. Notice there are three tabs on the top. We will go over them one by one. 
-                       Keep it at <b>vs. Year</b> for now and click next to get started.")
-  )$
+  el = "viz-wrapper",
+  title = "Visualization",
+  description = HTML("Welcome to our RMBL phenology visualization tool!<br><br>
+                        Notice there are three tabs on the top and we will go over them one by one.<br><br>
+                       So, keep it at <b>'vs. Year'</b> for now and click <b>Next</b> to get started.")
+)$
   step(
-    el = "plot1-wrapper",
-    title = "vs. Year plot",
-    description = HTML("In the first tab, the plot shows the change in snow conditions or phenology over the years.<br> 
-                       Hovering over the point on the far left shows you that in 1976, snow melted at day 136, which corresponds to sometime in the middle of May.<br>
-                       The trend line tracks how the snow melt date has changed over the years. 
+  el = "plot1-wrapper",
+  title = "vs. Year plot",
+  description = HTML("In the first tab, the plot shows the change in snow conditions and phenology over the years.<br><br>
+                       - Hovering over the point on the far left shows you that in 1976, snow melted at the day 136, which corresponds to sometime in the middle of May.<br><br>
+                       - The trend line tracks how the snow melt date has changed over the years.
                        There are fluctuations, but overall, it doesn't look like the snow melt date had changed much between 1976 and 1999.")
-  )$
+)$
   step(
-    el = "inputs1-wrapper",
-    title = "Selecting parameters",
-    description = "This tells you what parameters are depicted on the plot. Be careful with the axes since they can differ between the parameters. Keep them as they are for now."
-  )$
+  el = "inputs1-wrapper",
+  title = "Selecting parameters",
+  description = "This tells you what parameters are depicted on the plot. <br><br>
+                Note: Be careful with the axes since they can differ between the parameters. Keep them as they are for now."
+)$
   step(
-    el = "period-wrapper",
-    title = "Period",
-    description = HTML("This shows you the years that are currently plotted.<br>
-                       Comparing the years before 2000 and after 2000 is useful to see if the trend had held or not."),
-    position = "top"
-  )$
+  el = "period-wrapper",
+  title = "Period",
+  description = HTML("You can choose different time period you want the plot to be shown.<br><br>
+                      Recommended: Select <b>2000-2010</b> by clicking on the dropdown menu."),
+  position = "top"
+)$
   step(
-    el = "trendline",
-    title = "Trend line",
-    description = "Clicking here shows and hides trendlines on the plot. Keep it on for now and hit next."
-  )$
+  el = "trendline",
+  title = "Trend line",
+  description = "Clicking here shows or hides trendlines on the plot.
+                In this case, we keep it on for better visualization."
+)$
   step(
-    el = "stats1-wrapper",
-    title = "Stats",
-    description = HTML("The statistics appears when the trendline is present.<br>
+  el = "stats1-wrapper",
+  title = "Stats",
+  description = HTML("The statistics appears when the trendline is present.<br>
                        The value of the slope tells us that the snow melt date delayed by 0.1 day per year.
-                       The p-value of 0.78 suggests that the snow melt date had not significantly changed between 1974 and 1999. 
+                       The p-value of 0.78 suggests that the snow melt date had not significantly changed between 1974 and 1999.
                        The R<sup>2</sup> value shows us that only 0.35% of the variation in snow melt is explained by years.")
-  )$
+)$
   step(
-    el = "tabs",
-    title = "Next up...",
-    description = HTML("Now, let's move on to the second tab. Click on <b>vs. snow conditions</b> before hitting next.")
-  )$
+  el = "tabs",
+  title = "Next up...",
+  description = HTML("Now, let's move on to the second tab. Click on <b>vs. snow conditions</b> before hitting next.")
+)$
   step(
-    el = "plot2-wrapper",
-    title = "vs. Snow conditions plot",
-    description = HTML("In the second tab, the plot shows how the species phenology has shifted in relation to snow conditions.<br>
+  el = "plot2-wrapper",
+  title = "vs. Snow conditions plot",
+  description = HTML("In the second tab, the plot shows how the species phenology has shifted in relation to snow conditions.<br>
                        Each point represents a year. At a quick glance, we can see that the Yellow-bellied marmots are sighted later in the year when the snow sticks around until later.")
-  )$
+)$
   step(
-    el = "sidebar-wrapper",
-    title = "Sidebar",
-    description = "The parameters are the same as in the first tab."
-  )$
+  el = "sidebar-wrapper",
+  title = "Sidebar",
+  description = "The parameters are the same as in the first tab."
+)$
   step(
-    el = "stats2-wrapper",
-    title = "Stats",
-    description = "Here, it shows that the Yellow-bellied marmots are sighted 0.74 days later per day of delay in snow melt date.
+  el = "stats2-wrapper",
+  title = "Stats",
+  description = "Here, it shows that the Yellow-bellied marmots are sighted 0.74 days later per day of delay in snow melt date.
                   We can see that the p-value is colored red, meaning the trend is significant as the value is less than 0.05.
                   As much as 34% of the variation in Yellow-bellied marmots' sighting date is explained by the variation in the snow melt date as well."
-  )$
+)$
   step(
-    el = "tabs",
-    title = "Next up...",
-    description = HTML("Now, let's look at the last tab. Click on <b>vs. other weather data</b> and then hit next.")
-  )$
+  el = "tabs",
+  title = "Next up...",
+  description = HTML("Now, let's look at the last tab. Click on <b>vs. other weather data</b> and then hit next.")
+)$
   step(
-    el = "plot3-wrapper",
-    title = "vs. Other weather data plot",
-    description = HTML("In the last tab, the plot shows how the species phenology has shifted in relation to some environmental variables other than snow.<br>
+  el = "plot3-wrapper",
+  title = "vs. Other weather data plot",
+  description = HTML("In the last tab, the plot shows how the species phenology has shifted in relation to some environmental variables other than snow.<br>
                        It looks like the Yellow-bellied marmots tend to appear earlier in warmer springs.")
-  )
+)
--- ui.R
+++ ui.R
@@ -7,142 +7,166 @@ library(plotly)
 library(htmltools)
 # library(shinyBS)
 library(magrittr)
-#library(glouton)
+# library(glouton)
 library(cicerone)
 library(shinyjs)
 library(shinyBS)
 
-species <- c("Yellow-bellied marmot", "Least chipmunk", "American robin", "Steller's jay", 
-             "Red-winged blackbird", "Dark-eyed junco", "Northern flicker", "Tree swallow", "Red-naped sapsucker", "Fox sparrow", "Ruby-crowned kinglet", 
-             "Yellow-rumped warbler", "Cliff swallow", "Golden-mantled ground squirrel", "Broad-tailed hummingbird", "White-crowned sparrow", 
-             "Brown-headed cowbird", "Mountain bluebird", "Yellow warbler", "Tall-fringed bluebell", "Glacier lily", "Western spring beauty")
+species <- c(
+  "Yellow-bellied marmot", "Least chipmunk", "American robin", "Steller's jay",
+  "Red-winged blackbird", "Dark-eyed junco", "Northern flicker", "Tree swallow", "Red-naped sapsucker", "Fox sparrow", "Ruby-crowned kinglet",
+  "Yellow-rumped warbler", "Cliff swallow", "Golden-mantled ground squirrel", "Broad-tailed hummingbird", "White-crowned sparrow",
+  "Brown-headed cowbird", "Mountain bluebird", "Yellow warbler", "Tall-fringed bluebell", "Glacier lily", "Western spring beauty"
+)
 vars <- c("Mean minimum April temperature (°C)", "Mean maximum April temperature (°C)", "Melt water (mm)")
 
 
 
-shinyUI <- fluidPage(id = "page",
+shinyUI <- fluidPage(
+  id = "page",
   use_cicerone(),
   useShinyjs(),
   theme = shinytheme("united"),
-  setBackgroundColor(color = "#C7DAE0"), 
-  title="High-altitude Phenology",
+  setBackgroundColor(color = "white"),
+  title = "High-altitude Phenology",
+  tags$head(
+    tags$link(rel = "icon", href = "favicon.ico", type = "image/x-icon")  ),
   titlePanel(
-    div(tags$img(src="TrenchEdLogo.png", height = 150), 
-        "Climate Change and High-altitude Phenology")
+    div(
+      style = "display: flex; justify-content: center; align-items: center; background-color: #367fa9; color: white; padding: 10px; border-radius: 5px; width: 100%;",
+      tags$img(src = "TrenchEdLogo.png", height = 80),
+      tags$h1("High-altitude Phenology")
+    )
   ),
   includeHTML("intro.html"),
   actionBttn(
     inputId = "reset",
-    label = "Reset", 
+    label = "Reset",
     style = "material-flat",
     color = "danger",
     size = "xs"
   ),
   bsTooltip("reset", "If you have already changed the variables, reset them to default here before starting the tour."),
-  
   actionBttn(
     inputId = "tour",
-    label = "Take a tour!", 
+    label = "Take a tour!",
     style = "material-flat",
     color = "success",
     size = "sm"
-  ), 
+  ),
   br(),
   br(),
-  
-  div(id = "viz-wrapper",
-    tabsetPanel(id = "tabs",
-      tabPanel("vs. Year",
-              sidebarLayout(
-                sidebarPanel(
-                  id = "sidebar",
-                  div(
-                    id = "period-wrapper",
-                    selectInput(inputId = "period", "Period", c("1974-1999", "2000-2010", "1974-2010"))
-                  ),
-                  div(
-                    id = "inputs1-wrapper",
-                    pickerInput("snow", "Snow condition", choices = c("", "Snow melt date (JD)", "Annual snowfall (cm)", "Average snowpack (cm)"), 
-                                  options = list(style = "btn-success"), selected = "Snow melt date (JD)"),
-                    pickerInput("species", "Organism", choices = c("", list("Hibernating animals" = species[c(1,2)], 
-                                                                           "Migratory animals" = species[3:19],
-                                                                           "Plants" = species[20:22])), 
-                                options = list(style = "btn-success"))
-                  ),
-                  div(
-                    id = "trendline",
-                    materialSwitch("trend", "Trend line", value = TRUE, status = "danger")
-                  ),
-                  radioButtons("style", "Style", choices = c("Points", "Lines"), inline = TRUE)
-                ),
-                
-                mainPanel(
-                  div(
-                    id = "plot1-wrapper", 
-                    plotlyOutput("plot1")
-                  ),
-                  div(
-                    id = "stats1-wrapper",
-                    htmlOutput("stats1")
-                  ),
-                  br(),
-                  hr()
-                )
+  div(
+    id = "viz-wrapper",
+    tabsetPanel(
+      id = "tabs",
+      tabPanel(
+        "vs. Year",
+        sidebarLayout(
+          sidebarPanel(
+            id = "sidebar",
+            div(
+              id = "period-wrapper",
+              selectInput(inputId = "period", "Period", c("1974-1999", "2000-2010", "1974-2010"))
+            ),
+            div(
+              id = "inputs1-wrapper",
+              pickerInput("snow", "Snow condition",
+                choices = c("", "Snow melt date (JD)", "Annual snowfall (cm)", "Average snowpack (cm)"),
+                options = list(style = "btn-success"), selected = "Snow melt date (JD)"
+              ),
+              pickerInput("species", "Organism",
+                choices = c("", list(
+                  "Hibernating animals" = species[c(1, 2)],
+                  "Migratory animals" = species[3:19],
+                  "Plants" = species[20:22]
+                )),
+                options = list(style = "btn-success")
               )
+            ),
+            div(
+              id = "trendline",
+              materialSwitch("trend", "Trend line", value = TRUE, status = "danger")
+            ),
+            radioButtons("style", "Style", choices = c("Points", "Lines"), inline = TRUE)
+          ),
+          mainPanel(
+            div(
+              id = "plot1-wrapper",
+              plotlyOutput("plot1")
+            ),
+            div(
+              id = "stats1-wrapper",
+              htmlOutput("stats1")
+            ),
+            br(),
+            hr()
+          )
+        )
       ),
-      
-      tabPanel("vs. snow conditions", 
-               sidebarLayout(
-                 sidebarPanel(
-                   div(
-                     id = "sidebar-wrapper",
-                     selectInput("period2", "Period", c("1974-1999", "2000-2010", "1974-2010")),
-                     pickerInput("snow2", "Snow condition", choices = c("Snow melt date (JD)", "Annual snowfall (cm)", "Average snowpack (cm)"), 
-                                 options = list(style = "btn-success"), selected = "Snow melt date (JD)"),
-                     pickerInput("species2", "Organism", choices = list("Hibernating animals" = species[c(1,2)], 
-                                                                            "Migratory animals" = species[3:19],
-                                                                            "Plants" = species[20:22]), 
-                                 options = list(style = "btn-success")),
-                     materialSwitch("trend2", "Trend line", value = TRUE, status = "danger")
-                   )
-                 ),
-                 mainPanel(
-                   div(
-                     id = "plot2-wrapper", 
-                     plotlyOutput("plot2")
-                   ),
-                   div(
-                     id = "stats2-wrapper",
-                     htmlOutput("stats2")
-                   ),
-                   hr()
-                 )
-               ),
+      tabPanel(
+        "vs. snow conditions",
+        sidebarLayout(
+          sidebarPanel(
+            div(
+              id = "sidebar-wrapper",
+              selectInput("period2", "Period", c("1974-1999", "2000-2010", "1974-2010")),
+              pickerInput("snow2", "Snow condition",
+                choices = c("Snow melt date (JD)", "Annual snowfall (cm)", "Average snowpack (cm)"),
+                options = list(style = "btn-success"), selected = "Snow melt date (JD)"
+              ),
+              pickerInput("species2", "Organism",
+                choices = list(
+                  "Hibernating animals" = species[c(1, 2)],
+                  "Migratory animals" = species[3:19],
+                  "Plants" = species[20:22]
+                ),
+                options = list(style = "btn-success")
+              ),
+              materialSwitch("trend2", "Trend line", value = TRUE, status = "danger")
+            )
+          ),
+          mainPanel(
+            div(
+              id = "plot2-wrapper",
+              plotlyOutput("plot2")
+            ),
+            div(
+              id = "stats2-wrapper",
+              htmlOutput("stats2")
+            ),
+            hr()
+          )
+        ),
       ),
-      
-      tabPanel("vs. other weather data",
-               sidebarLayout(
-                 sidebarPanel(
-                   selectInput("period3", "Period", c("1974-1999", "2000-2010", "1974-2010")),
-                   pickerInput("weather", "Weather condition", choices = vars,
-                               options = list(style = "btn-success")),
-                   pickerInput("species3", "Organism", choices = list("Hibernating animals" = species[c(1,2)],
-                                                                     "Migratory animals" = species[3:19],
-                                                                     "Plants" = species[20:22]),
-                               options = list(style = "btn-success")),
-                   materialSwitch("trend3", "Trend line", value = TRUE, status = "danger")
-                 ),
-                 mainPanel(
-                   div(
-                     id = "plot3-wrapper",
-                     plotlyOutput("plot3")
-                   ),
-                   
-                   htmlOutput("stats3"),
-                   
-                   hr()
-                 )
-               )
+      tabPanel(
+        "vs. other weather data",
+        sidebarLayout(
+          sidebarPanel(
+            selectInput("period3", "Period", c("1974-1999", "2000-2010", "1974-2010")),
+            pickerInput("weather", "Weather condition",
+              choices = vars,
+              options = list(style = "btn-success")
+            ),
+            pickerInput("species3", "Organism",
+              choices = list(
+                "Hibernating animals" = species[c(1, 2)],
+                "Migratory animals" = species[3:19],
+                "Plants" = species[20:22]
+              ),
+              options = list(style = "btn-success")
+            ),
+            materialSwitch("trend3", "Trend line", value = TRUE, status = "danger")
+          ),
+          mainPanel(
+            div(
+              id = "plot3-wrapper",
+              plotlyOutput("plot3")
+            ),
+            htmlOutput("stats3"),
+            hr()
+          )
+        )
       )
     )
   )
